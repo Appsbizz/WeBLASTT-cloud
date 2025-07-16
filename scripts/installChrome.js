f0c5b705e94ed06244389eae49ca13cb0308c6d5
Appsbizz <appsbizz.user@gmail.com>
2025-07-16 08:10:16 +0800

    Create installChrome.js

---
+++ scripts/installChrome.js
@@ -0,0 +1,12 @@
+const { install } = require('@puppeteer/browsers');
+
+install({
+  browser: 'chrome',
+  buildId: '127.0.6533.88',
+  cacheDir: '/opt/render/.cache/puppeteer',
+})
+  .then(() => console.log("✅ Chrome installed"))
+  .catch(err => {
+    console.error("❌ Chrome install failed:", err.message);
+    process.exit(1);
+  });
